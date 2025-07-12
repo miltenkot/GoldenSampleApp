d1ccc8957c6a44c92d7f79bb2cd98b357bcb55a6
Bartlomiej Lanczyk <bartlomiejl@backbase.com>
2025-07-12 10:40:12 +0200

    Add default golden sample features navigation

--- GoldenSampleApp/AppFeature/Sources/AppFeature/AppFeature.swift
+++ GoldenSampleApp/AppFeature/Sources/AppFeature/AppFeature.swift
@@ -1,12 +1,33 @@
 import SwiftUI
 
 public struct AppView: View {
+    @State private var preferredColumn: NavigationSplitViewColumn = .detail
+    
     public init() {
         
     }
     
     public var body: some View {
-        Text("Hello, World!")
+        NavigationSplitView(preferredCompactColumn: $preferredColumn) {
+            List {
+                Section {
+                    ForEach(NavigationOptions.mainPages) { page in
+                        NavigationLink(value: page) {
+                            Label(page.name, systemImage: page.symbolName)
+                        }
+                    }
+                }
+            }
+            .navigationDestination(for: NavigationOptions.self) { page in
+                NavigationStack {
+                    page.viewForPage()
+                }
+            }
+        } detail: {
+            NavigationStack {
+                NavigationOptions.requests.viewForPage()
+            }
+        }
     }
 }
 
---
+++ GoldenSampleApp/AppFeature/Sources/AppFeature/Navigation.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+/// An enumeration of navigation options in the app.
+enum NavigationOptions: Equatable, Hashable, Identifiable, CaseIterable {
+    var id: Self { self }
+    /// A case that represents viewing the dealing with api requests
+    case requests
+    
+    static let mainPages: [NavigationOptions] = NavigationOptions.allCases
+    
+    var name: LocalizedStringResource {
+        switch self {
+        case .requests: return LocalizedStringResource("Requests", comment: "Title for the Requests tab, shown in the sidebar.")
+        }
+    }
+    
+    var symbolName: String {
+        switch self {
+        case .requests: "server.rack"
+        }
+    }
+    
+    /// A view builder that the split view uses to show a view for the selected navigation option.
+    @MainActor @ViewBuilder func viewForPage() -> some View {
+        switch self {
+        case .requests: Text("Requests")
+        }
+    }
+}
