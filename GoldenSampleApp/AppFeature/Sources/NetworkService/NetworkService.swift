8e5872c773b5dd91849a9974554aff91a2037c0a
Bartlomiej Lanczyk <bartlomiejl@backbase.com>
2025-07-28 17:19:57 +0200

    Small adjustments HTTPMethod + setJSONContentType

--- GoldenSampleApp/AppFeature/Sources/NetworkService/NetworkService.swift
+++ GoldenSampleApp/AppFeature/Sources/NetworkService/NetworkService.swift
@@ -13,35 +13,41 @@ public protocol NetworkServiceProtocol {
 
 @MainActor
 public final class NetworkService: NetworkServiceProtocol {
-    let session: URLSession
+    private let session: URLSession
+    private let encoder: JSONEncoder
+    private let decoder: JSONDecoder
     
-    public init(session: URLSession = URLSession.shared) {
+    public init(session: URLSession = .shared,
+                encoder: JSONEncoder = JSONEncoder(),
+                decoder: JSONDecoder = JSONDecoder()) {
         self.session = session
+        self.encoder = encoder
+        self.decoder = decoder
     }
     
     public func get<T: Decodable>(_ url: URL) async throws(NetworkingError) -> T {
-        do {
-            let (data, response) = try await session.data(from: url)
-            try validate(response)
-            return try decode(data)
-        } catch {
-            throw map(error)
-        }
+        let request = URLRequest(url: url)
+        return try await perform(request)
     }
     
     public func get<T: Decodable>(_ url: URL, parameters: [String: String]) async throws(NetworkingError) -> T {
-        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        guard var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            throw NetworkingError.otherError(URLError(.badURL))
+        }
         components.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
-        // // components.url always returns a non-nil URL as long as the input URL is valid (non-nil)
-        return try await get(components.url!)
+        
+        guard let parameterizedURL = components.url else {
+            throw NetworkingError.otherError(URLError(.badURL))
+        }
+        return try await get(parameterizedURL)
     }
     
     public func post<T: Encodable>(_ url: URL, body: T) async throws(NetworkingError) {
         do {
             var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.httpBody = try JSONEncoder().encode(body)
+            request.httpMethod = HTTPMethod.post.rawValue
+            request.setJSONContentType()
+            request.httpBody = try encoder.encode(body)
             
             let (_, response) = try await session.data(for: request)
             try validate(response)
@@ -49,26 +55,39 @@ public final class NetworkService: NetworkServiceProtocol {
             throw map(error)
         }
     }
-    
-    // MARK: - Private Helpers
-    
-    private func validate(_ response: URLResponse) throws {
-        guard let httpResponse = response as? HTTPURLResponse,
-              (200...299).contains(httpResponse.statusCode) else {
-            // The forced unwrap (response as? HTTPURLResponse)! inside the else block cannot be nil, since the guard ensures response is an HTTPURLResponse. Thus, the case of it being nil does not occur.
-            throw NetworkingError.invalidStatusCode((response as? HTTPURLResponse)!.statusCode)
+}
+
+// MARK: - Private Helpers
+
+private extension NetworkService {
+    func perform<T: Decodable>(_ request: URLRequest) async throws(NetworkingError) -> T {
+        do {
+            let (data, response) = try await session.data(for: request)
+            try validate(response)
+            return try decode(data)
+        } catch {
+            throw map(error)
         }
     }
     
-    private func decode<T: Decodable>(_ data: Data) throws -> T {
+    func validate(_ response: URLResponse) throws {
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkingError.otherError(URLError(.badURL))
+        }
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw NetworkingError.invalidStatusCode(httpResponse.statusCode)
+        }
+    }
+    
+    func decode<T: Decodable>(_ data: Data) throws -> T {
         do {
-            return try JSONDecoder().decode(T.self, from: data)
+            return try decoder.decode(T.self, from: data)
         } catch let error as DecodingError {
             throw NetworkingError.decodingFailed(error)
         }
     }
     
-    private func map(_ error: Error) -> NetworkingError {
+    func map(_ error: Error) -> NetworkingError {
         switch error {
         case let error as URLError:
             return .requestFailed(error)
@@ -77,5 +96,11 @@ public final class NetworkService: NetworkServiceProtocol {
         default:
             return .otherError(error)
         }
+    }
+}
+
+private extension URLRequest {
+    mutating func setJSONContentType() {
+        self.setValue("application/json", forHTTPHeaderField: "Content-Type")
     }
 }
